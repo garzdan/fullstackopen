7681d56795cb3b6648587af40407e13912cd5315
Daniele Garziano <daniele.garziano@shippypro.com>
2024-10-09 11:43:39 +0200

    2.6: Phonebook, step 1

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+
+const Person = ({ name }) => {
+  return (
+    <p>{name}</p>
+  );
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]);
+
+  const [newName, setNewName] = useState('');
+
+  const handleNameChange = event => setNewName(event.target.value);
+
+  const handlePersonAdd = event => {
+    event.preventDefault();
+
+    setPersons([
+      ...persons,
+      {
+        name: newName
+      }
+    ]);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handlePersonAdd}>
+        <div>
+          name: <input onChange={handleNameChange} value={newName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person =>
+        <Person key={person.name} name={person.name} />
+      )}
+    </div>
+  );
+};
+
+export default App;
