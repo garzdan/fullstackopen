c6db45ae598df9d62822508ee26a72f3ed369fb5
Daniele Garziano <daniele.garziano@shippypro.com>
2024-10-11 12:35:36 +0200

    3.3: Phonebook backend, step 3

--- part3/phonebook/src/index.js
+++ part3/phonebook/src/index.js
@@ -38,6 +38,16 @@ app.get('/api/persons', (req, res) => {
   res.json(persons);
 });
 
+app.get('/api/persons/:id', (req, res) => {
+  const person = persons.find((person) => person.id === req.params.id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
 });
