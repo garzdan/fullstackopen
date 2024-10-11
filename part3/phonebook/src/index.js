bbce6ea57ea2aabde13d01152afad5106f68d85f
Daniele Garziano <daniele.garziano@shippypro.com>
2024-10-11 12:28:09 +0200

    3.2: Phonebook backend, step 2

--- part3/phonebook/src/index.js
+++ part3/phonebook/src/index.js
@@ -27,6 +27,13 @@ let persons = [
 
 const app = express();
 
+app.get('/info', (req, res) => {
+  res.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+  `);
+});
+
 app.get('/api/persons', (req, res) => {
   res.json(persons);
 });
